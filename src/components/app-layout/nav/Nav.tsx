06d917d3053cca15f5543f0d3047f89c8d7daaee
Petr Cibulka <cibulka.me@gmail.com>
2022-12-30 11:21:51 +0100

    fix: get logo from /icons folder for consistency

--- src/components/app-layout/nav/Nav.tsx
+++ src/components/app-layout/nav/Nav.tsx
@@ -2,15 +2,15 @@
 import React from 'react';
 import LocaleMenu from 'src/components/locale-menu/LocaleMenu';
 
-import Icon from 'public/logo/bird.svg';
 import LinkLocalizedClient from 'src/components/link-localized/LinkLocalizedClient';
+import { IconLogo } from 'src/icons';
 
 const Nav = (props: { title: string }) => (
   <nav className="relative z-10 coat-menu">
     <div className={['app-max-w-content', 'flex items-center justify-between', 'py-4'].join(' ')}>
       <LinkLocalizedClient href="/" className="flex items-center">
         <span className="flex w-8 h-8 mr-2">
-          <Icon />
+          <IconLogo />
         </span>
         <h1 className="text-xl font-bold">{props.title}</h1>
       </LinkLocalizedClient>
